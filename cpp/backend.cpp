6ee454b4c54d6d944ac410860508206ab86383da
Jam Sadiq <14935293+JAMSADIQ@users.noreply.github.com>
2021-07-02 16:57:12 +0200

    added new function to get coefficient rather sum.

--- cpp/backend.cpp
+++ cpp/backend.cpp
@@ -56,6 +56,58 @@ py::array_t<T> kernel_sum (const py::array_t<T>& X,
     return eval;
 }
 
+template <typename T>
+py::array_t<T> kernel_coeff (const py::array_t<T>& X,
+                           const py::array_t<T>& Y,
+                           const py::array_t<T>& invbw,
+                           const py::array_t<T>& norm) {
+    // Gather input shape info
+    auto bX = X.template unchecked<2>();
+    auto bY = Y.template unchecked<2>();
+    auto lenX = bX.shape(0);
+    auto ndim = bX.shape(1);
+    auto lenY = bY.shape(0);
+    auto ndimY = bY.shape(1);
+    auto binvbw = invbw.template unchecked<1>();
+    auto bnorm = norm.template unchecked<1>();
+
+    // Check data integrity
+    if (ndim != ndimY) {
+        throw std::runtime_error(
+            "Points in X and Y must have the same number of dimensions.");
+    }
+    if (lenX != binvbw.shape(0) || lenX != bnorm.shape(0)) {
+        throw std::runtime_error(
+            "X, invbw and norm must have the same lengths.");
+    }
+
+    // Set up output: shape (lenY)
+    py::array_t<T> eval({lenY, lenX});
+    auto beval = eval.template mutable_unchecked<2>();
+
+    // Get squared distances for all in Y to all in X and sum up the PDF value
+    // from the gaussian kernel PDF for each Y.
+    T diff_ij;
+    T dist2_ij;
+    for (unsigned int i = 0; i < lenY; ++i) {  // Loop over Y elements
+        //beval(i) = 0.;
+        for (unsigned int j = 0; j < lenX; ++j) {  // Loop over X elements
+            dist2_ij = 0.;
+            for (unsigned int k = 0; k < ndim; ++k) {  // Loop over features
+                diff_ij = bY(i, k) - bX(j, k);
+                dist2_ij += diff_ij * diff_ij;
+            }
+            // PDF_i = sum_{j in X} (norm_j * exp(-0.5 * dist2_ij * invbw_j^2)
+            //beval(i) += bnorm(j) * std::exp(-0.5 * dist2_ij *
+              //                              binvbw(j) * binvbw(j));
+            beval(i,j) = bnorm(j) * std::exp(-0.5 * dist2_ij *
+                                            binvbw(j) * binvbw(j));
+        }
+    }
+
+    return eval;
+}
+
 
 PYBIND11_PLUGIN(backend) {
     py::module m("backend", R"pbdoc(
@@ -68,6 +120,7 @@ PYBIND11_PLUGIN(backend) {
            :toctree: _generate
 
            kernel_sum
+           kerne_coeff [I added this]
     )pbdoc");
 
     auto docstr = R"pbdoc(
@@ -95,6 +148,11 @@ PYBIND11_PLUGIN(backend) {
                     eval : float array, shape (len(Y))
                         The probability from the KDE PDF for each point in `Y`.
                   )pbdoc";
+    // Define the actual template typess
+    m.def("kernel_coeff", &kernel_coeff<double>, docstr,
+          py::arg("X"), py::arg("Y"), py::arg("invbw"), py::arg("norm"));
+    m.def("kernel_coeff", &kernel_coeff<float>, "",
+          py::arg("X"), py::arg("Y"), py::arg("invbw"), py::arg("norm"));
 
     // Define the actual template typess
     m.def("kernel_sum", &kernel_sum<double>, docstr,
